5799536332abd35f8a1363d4ed1efeb8310c9609
Eugene Lupashko <ievgen.lupashko@engagepoint.ua>
2016-06-13 10:14:03 +0300

    bmw project

---
+++ dev/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ievgen.lupashko on 6/10/2016.
+ */
+var app = angular.module('bmw-page', []);
+
+
+
+app.controller('TabController', function(){
+    
+    this.currentTab = "read";
+});
